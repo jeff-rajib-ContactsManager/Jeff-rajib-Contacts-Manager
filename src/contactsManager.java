6a0ee61a39c5d335289cc002e15fbf2d15aeb3c5
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-30 15:02:30 -0600

    solveed some bonus questions

--- src/contactsManager.java
+++ src/contactsManager.java
@@ -1,9 +1,11 @@
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,10 +14,15 @@ public class contactsManager {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        List<String> r = Arrays.asList("Name       | Phone Number\n" + "--------------------------");
+        Path p = Paths.get("src/contacts.txt");
+        Files.write(p,r);
+
         String choice;
         do {
-            System.out.println(" 1. View contacts.\n" +
+            System.out.println("1. View contacts.\n" +
                     "2. Add a new contact.\n" +
                     "3. Search a contact by name.\n" +
                     "4. Delete an existing contact.\n" +
@@ -46,13 +53,14 @@ public class contactsManager {
         Scanner s = new Scanner(System.in);
         try {
             Path p = Paths.get("src/contacts.txt");
-            System.out.println("Enter your name?");
-            String x = s.nextLine();
-            System.out.println("Enter your phone number?");
-            String z = s.nextLine();
-            contactList.add(x + " | " + z + " |");
+            System.out.println("Enter new contact name?");
+            String newContact = s.nextLine();
+            System.out.println("Enter new contact phone number?");
+            String newNumber = s.nextLine();
+            String w = newNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
+            contactList.add(String.format("%1$-10s |%2$-15s |", newContact,w));
             Files.write(p, contactList, StandardOpenOption.APPEND);
-            System.out.printf("Added %s \n", x);
+            System.out.printf("Added %s \n", newContact);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -80,12 +88,14 @@ public class contactsManager {
             System.out.println("Which contact do you like to see?");
             String contact = s.nextLine();
             Path p = Paths.get("src/contacts.txt");
-
+//          List<String> newArray = new ArrayList<>();
             List<String> data = Files.readAllLines(p);
 
             for (int i = 0; i < data.size(); i += 1) {
                 if(data.get(i).contains(contact)){
                     System.out.println(data.get(i));
+//                  newArray.add(data.get(i));
+//                  Files.write(p, newArray);
                 }
 
             }
